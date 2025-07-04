3b8e0d6b4c95966e662460cc4c86b01ae0f04b3e
iniadittt <aditya.braincore@gmail.com>
2025-07-04 19:33:43 +0700

    sync

--- app/constant.ts
+++ app/constant.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:9004";
+export const BACKEND_URL = "http://144.126.241.11:9002";
 export const TOKEN_EXPIRED = 7 * 24 * 60 * 60 * 1000;
 export const ALERT_DIAM = 5;
